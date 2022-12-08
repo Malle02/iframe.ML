4f7b50b7dae48335ebd9ae9f5d213eb7d0c76613
Malle02 <114107017+Malle02@users.noreply.github.com>
2022-12-08 14:16:09 +0100

    Add files via upload

--- site.js
+++ site.js
@@ -8,6 +8,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
@@ -17,3 +18,5 @@ document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () 
     hamburger.classList.remove(active)
     navMenu.classList.remove(active)
 }))
+
+
